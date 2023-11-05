bd0b2728cfb4197c8eed7b4e74628b86ad76cd35
DevDivyansh12115031 <122688947+DevDivyansh12115031@users.noreply.github.com>
2023-11-05 18:32:27 +0530

    Add files via upload

---
+++ question.js
@@ -0,0 +1,60 @@
+let questions = [
+    {
+        numb: 1,
+        question: "what does HTML stands for?",
+        answer: "c. Hyper Text Markup Language",
+        options: [
+            "A. Hyper Type Multi Language",
+            "B. Hyper Text Multiple Language",
+            "c. Hyper Text Markup Language",
+            "d. Home Text Multi Language"
+
+        ]
+    },
+    {
+        numb: 2,
+        question: "what does XML stands for?",
+        answer: "c. Hyper Text Markup Language",
+        options: [
+            "A. Hyper Type Multi Language",
+            "B. Hyper Text Multiple Language",
+            "c. Hyper Text Markup Language",
+            "d. Home Text Multi Language"
+
+
+        ]
+    },
+    {
+        numb: 3,
+        question: "what does HTML stands for?",
+        answer: "c. Hyper Text Markup Language",
+        options: [
+            "A. Hyper Type Multi Language",
+            "B. Hyper Text Multiple Language",
+            "c. Hyper Text Markup Language",
+            "d. Home Text Multi Language"
+        ]
+    },
+    {
+        numb: 4,
+        question: "what does HTML stands for?",
+        answer: "c. Hyper Text Markup Language",
+        options: [
+            "A. Hyper Type Multi Language",
+            "B. Hyper Text Multiple Language",
+            "c. Hyper Text Markup Language",
+            "d. Home Text Multi Language"
+        ]
+    },
+    {
+        numb: 5,
+        question: "what does HTML stands for?",
+        answer: "c. Hyper Text Markup Language",
+        options: [
+            "A. Hyper Type Multi Language",
+            "B. Hyper Text Multiple Language",
+            "c. Hyper Text Markup Language",
+            "d. Home Text Multi Language"
+        ]
+    }
+];
